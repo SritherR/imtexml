2f724155a08c70f51f1d7b650f03e0fda4ac1db5
SritherR <64246613+SritherR@users.noreply.github.com>
2020-04-26 19:47:19 +0530

    Update settings.py

--- imgupload/settings.py
+++ imgupload/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIAFILES_DIRS=[
+STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'media')
     ]
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static') 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
