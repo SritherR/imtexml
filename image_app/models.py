5c746be2708ebaae01323baf501445934d5343b8
Srither R <srither.r@alight.com>
2020-04-28 11:29:01 +0530

    deleted

--- image_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Hotel(models.Model): 
-    name = models.CharField(max_length=50,null=True) 
-
-    
-    Image = models.ImageField(upload_to='images/',blank=True,null=True) 
-    def __img__(self):
-        return self.Image
