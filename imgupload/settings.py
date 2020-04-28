73461b4e023a9d03aaf96c9565e1628a166dfeb0
SritherR <64246613+SritherR@users.noreply.github.com>
2020-04-28 18:06:49 +0530

    Update settings.py

--- imgupload/settings.py
+++ imgupload/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zs0jwte2b-#!g5w-*w^6inytt1o_0+g=h9^e*p((z72qhdq1$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
