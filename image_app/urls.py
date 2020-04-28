f34cecabca3a40f7b65445f408ad160d534a9925
Srither R <srither.r@alight.com>
2020-04-28 11:33:56 +0530

    update

---
+++ image_app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin 
+from django.urls import path 
+from django.conf import settings 
+from django.conf.urls.static import static 
+from .views import *
+  
+urlpatterns = [ 
+    path('', hotel_image_view, name = 'image'), 
+    path('Image_details', success, name = 'success'), 
+ 
+] 
+  
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
